e6de4b29f365a25470f7bbe986735a5ff3296403
Magnata2011 <lordreaper2000yt@gmail.com>
2026-09-07 11:00:41 -0300

    Change TOTAL_NUMEROS from 999 to 500

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ const RIFA_CONFIG = {
     // QUANTIDADE TOTAL DE NÚMEROS DA RIFA
     // -----------------------------------------------------------
 
-    TOTAL_NUMEROS: 999,
+    TOTAL_NUMEROS: 500,
 
 
     // -----------------------------------------------------------
